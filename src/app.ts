e04a8590a02904fbd393a9fd075937825db432be
Silvio Ronaldo <silvio.ronaldo77@gmail.com>
2021-01-13 19:01:46 -0300

    Configurando metodos chamados por construtor

---
+++ src/app.ts
@@ -0,0 +1,46 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+import userRoute from './routes/user.route';
+
+class App {
+    private express: express.Application;
+    private port = 3333;
+
+    constructor() {
+        this.express = express();
+        this.middlewares();
+        this.database();
+        this.routes();
+        this.listen();
+    }
+
+    private middlewares(): void {
+        this.express.use(express.json());
+        this.express.use(cors());
+    }
+
+    private database(): void {
+        mongoose.connect('mongodb+srv://silvio:yLxkd2MtN4BPDMqV@cluster0.udcjj.mongodb.net/test?retryWrites=true&w=majority', {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+        });
+    }
+
+    private routes(): void {
+        this.express.use('/users', userRoute);
+    }
+
+    private listen(): void {
+        this.express.listen(this.port, () => {
+            console.log(`This app is running on port ${this.port}`);
+        });
+    }
+
+    public getApp(): express.Application {
+        return this.express;
+    }
+}
+
+export default App;
